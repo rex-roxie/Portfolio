34bed5978018ce7ff92d2c93789b3dcab3360fec
rex-roxie <rex.roxie@outlook.com>
2021-12-23 13:30:38 +0300

    finish basic header styling

---
+++ index.js
@@ -0,0 +1,25 @@
+const nav = document.getElementById("nav");
+const bars = document.getElementById("bars");
+const x = document.getElementById("x");
+const rest_of_body = document.getElementById("rest-of-body");
+let isNavOpen = false;
+
+const OpenNav = () => {
+    nav.style.display = "flex";
+    nav.style.opacity = "70%";
+    bars.style.display = "none";
+    x.style.display = "block";
+    isNavOpen = true;
+}
+
+const CloseNav = () => {
+    nav.style.display = "none";
+    bars.style.display = "block";
+    x.style.display = "none";
+    isNavOpen = false;
+}
+
+bars.onclick = OpenNav;
+x.onclick = CloseNav;
+
+rest_of_body.addEventListener("mousedown", CloseNav);
